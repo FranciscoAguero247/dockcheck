41c5cce4da24fbb0831b5b4083e328cd588f8a33
FranciscoAguero247 <blast255@live.com>
2026-08-14 21:00:06 -0600

    Fix: Move mock environment variables to Jest setup file

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,3 @@
 import '@testing-library/jest-dom';
+process.env.NEXT_PUBLIC_SUPABASE_URL = 'https://supabase.co';
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.dummy_token';
